ea09fb5ae04f71a343c7dfbc8f030d3b74f3d93c
kohdai13 <kohdai6013@gmail.com>
2022-03-09 03:20:59 +0000

    Add コメント機能

--- app/controllers/shop_comments_controller.rb
+++ app/controllers/shop_comments_controller.rb
@@ -1,2 +1,22 @@
 class ShopCommentsController < ApplicationController
+
+  def create
+    shop = Shop.find(params[:shop_id])
+    comment = current_user.shop_comments.new(shop_comment_params)
+    comment.shop_id = shop.id
+    comment.save
+    redirect_to shop_path(shop)
+  end
+
+  def destroy
+    ShopComment.find(params[:id]).destroy
+    redirect_to shop_path(params[:shop_id])
+  end
+
+  private
+
+  def shop_comment_params
+    params.require(:shop_comment).permit(:comment)
+  end
+
 end
