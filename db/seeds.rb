4be43810290ccf1236b501ff2855d68fe4fb14c8
kohdai13 <kohdai6013@gmail.com>
2022-03-17 09:37:14 +0000

    Add seed.rb テストユーザー

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "テスト",
+  nickname: "テスト",
+  introduction: "よろしくお願いします",
+  email: "test@test.com",
+  password: "123456",
+  password_confirmation: "123456"
+  )
