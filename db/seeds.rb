6d190936fc8ada24855fce7ad5b6d5614d1a1c0f
kohdai13 <kohdai6013@gmail.com>
2022-03-20 06:38:13 +0000

    Fix seedファイル

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
+user = User.create!(
+  id: 1,
   name: "テスト",
   nickname: "テスト",
   introduction: "よろしくお願いします",
@@ -14,3 +15,41 @@ User.create!(
   password: "123456",
   password_confirmation: "123456"
   )
+  user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user1.png')),
+                  filename: 'user1.png')
+
+user2 = User.create!(
+  id: 2,
+  name: "令和智子",
+  nickname: "ともちん",
+  introduction: "服が大好きです！",
+  email: "tomoko@tomoko.com",
+  password: "123456",
+  password_confirmation: "123456"
+  )
+  user2.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user2.png')),
+                  filename: 'user2.png')
+
+user3 = User.create!(
+  id: 3,
+  name: "山田太郎",
+  nickname: "ヤマ",
+  introduction: "古着が好きです！",
+  email: "tarou@tarou.com",
+  password: "123456",
+  password_confirmation: "123456"
+  )
+  user3.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user3.png')),
+                  filename: 'user3.png')
+
+user4 = User.create!(
+  id: 4,
+  name: "鈴木一郎",
+  nickname: "キイチ",
+  introduction: "セレショによく行きます！",
+  email: "ichirou@ichirou.com",
+  password: "123456",
+  password_confirmation: "123456"
+  )
+  user4.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user4.png')),
+                  filename: 'user4.png')
