1acbe2c1b820c17be1aed78eb6680d7ace560e6d
kohdai13 <kohdai6013@gmail.com>
2022-03-22 01:55:06 +0000

    Fix ランキング機能　１０位まで表示

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,6 +1,6 @@
 class RanksController < ApplicationController
   def index
-    # 投稿のお気に入り数ランキング
-    @shop_favorite_ranks = Shop.find(Favorite.group(:shop_id).order('count(shop_id) desc').pluck(:shop_id))
+    # 投稿のお気に入り数ランキング 10位まで表示
+    @shop_favorite_ranks = Shop.find(Favorite.group(:shop_id).order('count(shop_id) desc').limit(10).pluck(:shop_id))
   end
 end
